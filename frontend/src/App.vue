b30784101c335c7d047e6a073dbecef5b6378f3c
Barakudum <68517540+Barakudum@users.noreply.github.com>
2025-06-21 16:58:41 +0200

    frontend: added sonner system

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
-
+import { Toaster } from "@/components/ui/sonner";
 </script>
 
 <template>
+  <!-- https://github.com/unovue/shadcn-vue/issues/462 -->
+  <Toaster rich-colors theme="dark" :visible-toasts="5" class="pointer-events-auto" />
   <router-view />
 </template>
