143e63bda79a2e308e6bd26837cc5de0991520b9
Barakudum <68517540+Barakudum@users.noreply.github.com>
2025-06-30 20:23:01 +0200

    backend: improved configuration for redis connection

--- backend/app/config/redis.py
+++ backend/app/config/redis.py
@@ -16,6 +16,11 @@ class RedisSettings(BaseSettings):
         description="Full redis connection URL (e.g., redis://localhost:6379)",
     )
 
+    RETRIES: int = Field(
+        default=3, ge=1,
+        description="Number of times to retry connecting to Redis",
+    )
+
     model_config = SettingsConfigDict(
         env_prefix="REDIS_",
         env_ignore_empty=True,
--- backend/app/core/redis.py
+++ backend/app/core/redis.py
@@ -3,10 +3,19 @@ r"""
 
 """
 import redis.asyncio as redis
+from redis.backoff import ExponentialBackoff
+from redis.retry import Retry
 from app.config import SETTINGS
 
 
 __all__ = ['redis_client']
 
 
-redis_client = redis.from_url(SETTINGS.REDIS.URL)
+redis_client = redis.from_url(
+    url=str(SETTINGS.REDIS.URL),
+    decode_responses=True,
+    retry=Retry(
+        retries=SETTINGS.REDIS.RETRIES,
+        backoff=ExponentialBackoff(),
+    ),
+)
