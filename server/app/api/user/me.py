228825f027e6b2642f4f92fe774a3e64a6cda0ed
Barakudum <68517540+Barakudum@users.noreply.github.com>
2025-07-07 18:23:06 +0200

    server: added email re-verification on PATH /user/me endpoint

--- server/app/api/user/me.py
+++ server/app/api/user/me.py
@@ -4,10 +4,11 @@ r"""
 """
 import typing as t
 import uuid
-from fastapi import APIRouter, Depends, Body
+from fastapi import APIRouter, Request, BackgroundTasks, Depends, Body
 import pydantic
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.core.dependencies import get_current_user, get_async_session
+from app.core.reusables.verification_mail import send_verification_email
 from app.db.models import User
 
 
@@ -36,18 +37,23 @@ class UserMePartialRequest(pydantic.BaseModel):
 
 @router.patch("/me", response_model=UserMeResponse)
 async def patch_me(
+        request: Request,
+        background_tasks: BackgroundTasks,
         session: AsyncSession = Depends(get_async_session),
         user: User = Depends(get_current_user),
         changed: UserMePartialRequest = Body()
 ):
     if 'email' in changed.model_fields_set:
         user.email = changed.email
-        # todo: re-validation request
+        user.email_verified = False
     if 'name' in changed.model_fields_set:
         user.name = changed.name
     if 'display_name' in changed.model_fields_set:
         user.display_name = changed.display_name
     await session.commit()
     await session.refresh(user)
+
+    if 'email' in changed.model_fields_set:
+        background_tasks.add_task(send_verification_email, request=request, user=user)
 
     return user
