10feb66ed08bbdb23e74ef00c81fdee9a04a3777
Barakudum <68517540+Barakudum@users.noreply.github.com>
2025-07-09 13:15:19 +0200

    server: improved SSE responses with sse-starlette

--- server/app/api/admin/logs/live.py
+++ server/app/api/admin/logs/live.py
@@ -6,6 +6,7 @@ import asyncio
 import structlog
 from fastapi import APIRouter, Request
 from fastapi.responses import StreamingResponse
+from sse_starlette import EventSourceResponse, ServerSentEvent
 from app.core.logging import REDIS_CHANNEL
 from app.core.util import get_client_ip
 from app.core.redis import redis_client
@@ -19,21 +20,24 @@ logger: structlog.BoundLogger = structlog.get_logger()
 async def logs_live(
         request: Request,
 ):
-    logger.info("logs-live", message="new listener connected", client=get_client_ip(request=request))
+    logger.info("new listener connected", client=get_client_ip(request=request))
 
+    return EventSourceResponse(event_stream(request=request), media_type="text/event-stream")
+
+
+async def event_stream(request: Request):
     pubsub = redis_client.pubsub()
     await pubsub.subscribe(REDIS_CHANNEL)
 
-    async def event_stream():
-        try:
-            while not await request.is_disconnected():
-                message = await pubsub.get_message(ignore_subscribe_messages=True, timeout=1)
-                if message:
-                    log_entry_json = message['data']
-                    yield f"data: {log_entry_json}\n\n"
-                await asyncio.sleep(0.01)
-        finally:
-            await pubsub.unsubscribe(REDIS_CHANNEL)
-            await pubsub.aclose()
-
-    return StreamingResponse(event_stream(), media_type="text/event-stream")
+    try:
+        while not await request.is_disconnected():
+            message = await pubsub.get_message(ignore_subscribe_messages=True, timeout=1)
+            if message:
+                log_entry_json = message['data']
+                yield ServerSentEvent(
+                    data=log_entry_json,
+                )
+            await asyncio.sleep(0.01)
+    finally:
+        await pubsub.unsubscribe(REDIS_CHANNEL)
+        await pubsub.aclose()
