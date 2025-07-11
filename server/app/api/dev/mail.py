f423660a7c414cc8e44fa091c73449396182aa38
Barakudum <68517540+Barakudum@users.noreply.github.com>
2025-07-11 12:14:55 +0200

    server: improved error handling for /api/dev/mail/test

--- server/app/api/dev/mail.py
+++ server/app/api/dev/mail.py
@@ -2,10 +2,10 @@
 r"""
 
 """
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, status, HTTPException
 from app.db.models import User
 from app.core.dependencies import get_current_user
-from app.services.mail import TestParameters, send_test_email
+from app.services.mail import SUPPORTED, TestParameters, send_test_email
 
 
 router = APIRouter(prefix="/mail")
@@ -15,6 +15,9 @@ router = APIRouter(prefix="/mail")
 async def post_mail_test(
         user: User = Depends(get_current_user),
 ):
+    if not SUPPORTED:
+        raise HTTPException(status_code=status.HTTP_503_SERVICE_UNAVAILABLE, detail="Mail service is not supported")
+
     await send_test_email(
         to=user.email,
         parameters=TestParameters(),
