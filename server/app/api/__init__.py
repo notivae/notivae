22e8816c107d65266ebdec97bf1496a036c2b2ae
Barakudum <68517540+Barakudum@users.noreply.github.com>
2025-07-12 21:42:49 +0200

    server: added endpoint to fetch supported features

--- server/app/api/__init__.py
+++ server/app/api/__init__.py
@@ -6,6 +6,7 @@ from fastapi import APIRouter
 from app.config import SETTINGS
 from .admin import router as admin_router
 from .auth import router as auth_router
+from .features import router as features_router
 from .security import router as security_router
 from .user import router as user_router
 
@@ -13,6 +14,7 @@ from .user import router as user_router
 router = APIRouter(prefix="/api")
 router.include_router(admin_router)
 router.include_router(auth_router)
+router.include_router(features_router)
 router.include_router(security_router)
 router.include_router(user_router)
 
---
+++ server/app/api/features.py
@@ -0,0 +1,46 @@
+# -*- coding=utf-8 -*-
+r"""
+
+"""
+from fastapi import APIRouter
+from pydantic import BaseModel
+from app.config import SETTINGS, AccountCreationMode
+from app.services.mail import SUPPORTED as MAIL_SUPPORTED
+from app.services.virus_scanning import SUPPORTED as VIRUS_SCANNING_SUPPORTED
+
+
+router = APIRouter()
+
+
+class AuthFeatures(BaseModel):
+    local: bool
+    oidc: bool
+
+
+class ServicesFeatures(BaseModel):
+    mail: bool
+    virus_scanning: bool
+
+
+class FeaturesResponse(BaseModel):
+    auth: AuthFeatures
+    services: ServicesFeatures
+    account_creation: AccountCreationMode
+
+
+@router.get("/features", response_model=FeaturesResponse)
+async def get_features():
+    r"""
+    fetch which features are supported
+    """
+    return FeaturesResponse(
+        auth=AuthFeatures(
+            local=SETTINGS.AUTH_LOCAL.ENABLED,
+            oidc=SETTINGS.OIDC is not None,
+        ),
+        services=ServicesFeatures(
+            mail=MAIL_SUPPORTED,
+            virus_scanning=VIRUS_SCANNING_SUPPORTED,
+        ),
+        account_creation=SETTINGS.APP.ACCOUNT_CREATION,
+    )
