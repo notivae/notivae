fa33c3dc5553569a7a510a5beef2d4c7192f3439
Barakudum <68517540+Barakudum@users.noreply.github.com>
2025-07-13 11:56:34 +0200

    web: added proxy to the server

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -6,6 +6,11 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
+  server: {
+    proxy: {
+      '/api': 'http://server:8000',
+    }
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
