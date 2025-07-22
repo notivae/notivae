3bc1b1f5ca644b90b7759d185c1bcb506333d97c
Barakudum <68517540+Barakudum@users.noreply.github.com>
2025-07-22 08:51:29 +0200

    server: modified logout to delete auth-session from the database

--- server/app/api/auth/logout.py
+++ server/app/api/auth/logout.py
@@ -3,7 +3,9 @@ r"""
 
 """
 from fastapi import APIRouter, Response, status, Depends
-from app.core.dependencies import rate_limited
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.core.dependencies import rate_limited, get_async_session, get_current_auth_session_optional
+from app.db.models import AuthSession
 
 
 router = APIRouter()
@@ -14,9 +16,15 @@ router = APIRouter()
     status_code=status.HTTP_204_NO_CONTENT,
     dependencies=[Depends(rate_limited(capacity=10, refill_rate=10/60))],
 )
-async def logout(response: Response) -> None:
+async def logout(
+        response: Response,
+        session: AsyncSession = Depends(get_async_session),
+        auth_session: AuthSession = Depends(get_current_auth_session_optional),
+) -> None:
 
-    # todo: remove session from database
+    if auth_session:
+        await session.delete(auth_session)
+        await session.commit()
 
     response.delete_cookie(
         key="session_token",
