216ab4ead450a5776dd9448c52199e374eaad16f
Barakudum <68517540+Barakudum@users.noreply.github.com>
2025-07-22 19:24:53 +0200

    server: made ws endpoint resilient against workers

--- server/app/core/redis.py
+++ server/app/core/redis.py
@@ -2,13 +2,14 @@
 r"""
 
 """
+import typing as t
 import redis.asyncio as redis
 from redis.backoff import ExponentialBackoff
 from redis.retry import Retry
 from app.config import SETTINGS
 
 
-__all__ = ['redis_client']
+__all__ = ['redis_client', 'PubSubMessage']
 
 
 redis_client = redis.from_url(
@@ -19,3 +20,10 @@ redis_client = redis.from_url(
         backoff=ExponentialBackoff(),
     ),
 )
+
+
+class PubSubMessage(t.TypedDict):
+    type: str
+    pattern: t.Optional[str]
+    channel: t.Optional[str]
+    data: t.Any
--- server/app/ws/__init__.py
+++ server/app/ws/__init__.py
@@ -2,17 +2,67 @@
 r"""
 
 """
-from fastapi import APIRouter, WebSocket
+import asyncio
+import structlog
+from fastapi import APIRouter, WebSocket, status, WebSocketDisconnect
+from app.core.redis import redis_client, PubSubMessage
 
 
 router = APIRouter()
+logger = structlog.get_logger()
 
 
 @router.websocket(path='/api/ws')
 async def websocket_endpoint(
         websocket: WebSocket,
 ):
+    logger.info(f"websocket connection opened")
     await websocket.accept()
-    while True:
-        data = await websocket.receive_text()
-        await websocket.send_text(f"Data: {data}")
+
+    # todo: authenticate
+    if False:
+        await websocket.close(code=status.WS_1008_POLICY_VIOLATION, reason="Missing authentication")
+        return
+
+    listener = asyncio.create_task(websocket_listener(websocket=websocket))
+    sender = asyncio.create_task(websocket_sender(websocket=websocket))
+
+    done, pending = await asyncio.wait(
+        [listener, sender],
+        return_when=asyncio.FIRST_COMPLETED,
+    )
+
+    for task in pending:
+        task.cancel()
+
+    if any(f.exception() for f in done):
+        for task in done:
+            exc = task.exception()
+            if exc:
+                logger.error(f"websocket closed unexpectedly ({type(exc)}: {exc})", exc_info=exc)
+        await websocket.close(code=status.WS_1011_INTERNAL_ERROR, reason="critical error")
+    else:
+        await websocket.close(code=status.WS_1000_NORMAL_CLOSURE, reason="WebSocket lifecycle end")
+
+
+async def websocket_listener(websocket: WebSocket):
+    try:
+        while True:
+            data = await websocket.receive_text()  # todo: validate against pydantic classes
+            await redis_client.publish(f"ws:data", data)  # todo: adjust channel
+    except WebSocketDisconnect:
+        pass
+
+
+async def websocket_sender(websocket: WebSocket):
+    try:
+        pubsub = redis_client.pubsub()
+        await pubsub.subscribe("ws:data")
+        while True:
+            message: PubSubMessage = await pubsub.get_message(ignore_subscribe_messages=True, timeout=10)
+            if message is None:
+                await websocket.send_json({ 'type': 'keepalive' })
+            else:
+                await websocket.send_text(message['data'])
+    except WebSocketDisconnect:
+        pass
