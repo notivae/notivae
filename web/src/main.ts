2da11f4b35d173a2f80af5566fc9fff73851d197
Barakudum <68517540+Barakudum@users.noreply.github.com>
2025-07-24 12:12:06 +0200

    web: added axios and pinia

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,9 +1,18 @@
 import './index.css';
 import 'vue-sonner/style.css';
+
 import { createApp } from 'vue';
+import { createPinia } from "pinia";
+import axios from "axios";
+
 import App from '@/App.vue';
 import router from "@/router";
 
+
+axios.defaults.withCredentials = true;
+
+
 createApp(App)
     .use(router)
+    .use(createPinia)
     .mount('#app');
