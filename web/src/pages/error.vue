273fc39a476a4594fe1cd55174e02c62fef0e119
Barakudum <68517540+Barakudum@users.noreply.github.com>
2025-07-29 09:40:27 +0200

    web: added simple error page

---
+++ web/src/pages/error.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import { useRoute } from "vue-router";
+import { LucideCircleX, LucideHouse, LucideArrowLeft } from "lucide-vue-next";
+import { Button } from "@/components/ui/button";
+
+definePage({
+  meta: {
+    requiresAuth: false,
+  }
+});
+
+const currentRoute = useRoute();
+</script>
+
+<template>
+  <main class="min-h-svh p-10 grid place-items-center">
+    <section class="text-center space-y-6 max-w-md">
+      <h1 class="text-5xl font-bold text-destructive-foreground">
+        <LucideCircleX class="size-12 inline-block" />
+        Error
+        <LucideCircleX class="size-12 inline-block" />
+      </h1>
+      <p class="text-xl font-semibold">You encountered a critical error.</p>
+      <p class="text-base text-muted-foreground font-mono">
+        {{ currentRoute.query.name ?? "Error" }}: {{ currentRoute.query.message ?? "~unknown~" }}
+      </p>
+      <div class="flex justify-center gap-4">
+        <router-link
+            :to="{ name: '/' }"
+        >
+          <Button>
+            <LucideHouse />
+            Go Home
+          </Button>
+        </router-link>
+        <Button
+            variant="secondary"
+            @click="$router.back()"
+        >
+          <LucideArrowLeft />
+          Go Back
+        </Button>
+      </div>
+    </section>
+  </main>
+</template>
