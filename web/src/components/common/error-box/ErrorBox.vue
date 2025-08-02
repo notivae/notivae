a65d6b9bc7800c4de6e05d222f502ed44e78673c
Barakudum <68517540+Barakudum@users.noreply.github.com>
2025-08-02 14:14:40 +0200

    web: added ErrorBox.vue component

---
+++ web/src/components/common/error-box/ErrorBox.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { AxiosError } from "axios";
+
+defineProps<{
+  error: unknown
+}>();
+</script>
+
+<template>
+  <p v-if="error !== null && error !== undefined" class="font-mono text-sm text-destructive-foreground">
+    <template v-if="error instanceof AxiosError">
+      ({{ error.response?.status }}: {{ error.response?.data.detail }})
+    </template>
+    <template v-else-if="error instanceof Error">
+      {{ error.name }}: {{ error.message }}
+    </template>
+    <template>
+      {{ error }}
+    </template>
+  </p>
+</template>
