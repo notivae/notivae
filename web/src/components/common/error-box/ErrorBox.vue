cda73364dc938b1233f7d8da8a50c25752594b79
Barakudum <68517540+Barakudum@users.noreply.github.com>
2025-08-05 11:03:57 +0200

    web: improved ErrorBox.vue

--- web/src/components/common/error-box/ErrorBox.vue
+++ web/src/components/common/error-box/ErrorBox.vue
@@ -1,15 +1,38 @@
 <script setup lang="ts">
-import { AxiosError } from "axios";
+import { AxiosError, HttpStatusCode } from "axios";
 
 defineProps<{
   error: unknown
 }>();
+
+function formatSeconds(seconds: number) {
+  if (seconds === undefined) return "~unknown~";
+  if (seconds < 60) return seconds.toString();
+  const mins = Math.floor(seconds / 60);
+  const secs = seconds % 60;
+  return `${mins}:${secs < 10 ? '0' : ''}${secs}`;
+}
 </script>
 
 <template>
   <p v-if="error !== null && error !== undefined" class="font-mono text-sm text-destructive-foreground">
     <template v-if="error instanceof AxiosError">
-      ({{ error.response?.status }}: {{ error.response?.data.detail }})
+      <template v-if="error.response?.status === HttpStatusCode.TooManyRequests">
+        You are currently doing too many requests.
+        Try again in {{ formatSeconds(error.response?.headers["retry-after"]) }}.
+      </template>
+      <template v-else-if="error.response?.status === HttpStatusCode.Conflict">
+        You stumbled across a conflict. Not sure how to handle that.
+      </template>
+      <template v-else-if="error.response?.status === HttpStatusCode.InternalServerError">
+        The server has encountered a situation it does not know how to handle.
+      </template>
+      <template v-else-if="error.response?.status === HttpStatusCode.ServiceUnavailable">
+        This service is currently unavailable.
+      </template>
+      <template v-else>
+        ({{ error.response?.status }}: {{ error.response?.data.detail }})
+      </template>
     </template>
     <template v-else-if="error instanceof Error">
       {{ error.name }}: {{ error.message }}
