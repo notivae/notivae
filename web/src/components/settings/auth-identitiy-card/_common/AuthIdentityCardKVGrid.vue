b0cf820a6a3477f5a1a3f4260bf7967028e9a92a
Barakudum <68517540+Barakudum@users.noreply.github.com>
2025-08-16 12:54:08 +0200

    web: AuthIdentityCardKVGrid.vue fixed overflow issue and added LucideMinus for undefined|null values

--- web/src/components/settings/auth-identitiy-card/_common/AuthIdentityCardKVGrid.vue
+++ web/src/components/settings/auth-identitiy-card/_common/AuthIdentityCardKVGrid.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { Label } from "@/components/ui/label";
+import { LucideMinus } from "lucide-vue-next";
 
 defineProps<{
   kv: Record<string, unknown>
@@ -9,8 +10,15 @@ defineProps<{
 <template>
   <div class="grid grid-cols-[auto_1fr] gap-x-4 gap-y-1">
     <template v-for="[key, value] in Object.entries(kv)">
-      <Label class="font-bold">{{ key }}</Label>
-      <span>{{ value }}</span>
+      <Label class="font-bold whitespace-nowrap">
+        {{ key }}
+      </Label>
+      <span v-if="value !== undefined && value !== null" class="break-words min-w-0">
+        {{ value }}
+      </span>
+      <span v-else class="text-muted-foreground">
+        <LucideMinus />
+      </span>
     </template>
   </div>
 </template>
